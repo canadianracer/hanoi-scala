d9c9d6c62134ea90f29d1f31b2f78f85147d3d96
Marc Morin <mmorin@auvik.com>
2013-06-10 12:39:23 -0400

    simple changes

--- src/main/scala/ca/morinfamily/hanoi/Hanoi.scala
+++ src/main/scala/ca/morinfamily/hanoi/Hanoi.scala
@@ -1,18 +1,17 @@
 package ca.morinfamily.hanoi
 
-
 object Hanoi {
     
     
-    def move[T](n: Int, from: T, to: T, via: T)(implicit next: List[(T,T)] = Nil): List[(T,T)] =
-        n match {
-          case 0 => next
-          case 1 => (from -> to) :: next
-          case _ => move(n - 1, from, via, to)  {
-                      move(1, from, to, via) {
-                        move(n-1, via, to, from)
-                      }
-                    }
-        }
+  def move[T](n: Int, from: T, to: T, via: T)(implicit next: List[(T,T)] = Nil): List[(T,T)] =
+    n match {
+      case 0 => next
+      case 1 => (from -> to) :: next
+      case _ => move(n - 1, from, via, to)  {
+                  move(1, from, to, via) {
+                    move(n-1, via, to, from)
+                  }
+                }
+    }
 }
 
